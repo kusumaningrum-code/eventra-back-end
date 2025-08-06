6b4341a2d7db483f8eb3ea7e03b422b3168b0da8
kusumaningrum-code <kusumaningrum416@gmail.com>
2025-08-07 04:07:24 +0700

    msg

--- src/utils/emailSender.ts
+++ src/utils/emailSender.ts
@@ -6,14 +6,16 @@ export const sendEmail = async (
   htmlContent: string
 ) => {
   try {
+    console.log("Mengirim email ke:", email);
     await transporter.sendMail({
       from: process.env.MAIL_SENDER,
       to: email,
       subject,
       html: htmlContent,
     });
+    console.log("Email berhasil dikirim");
   } catch (error) {
-    console.log(error);
+    console.log("Gagal kirim email:", error);
     throw error;
   }
 };
